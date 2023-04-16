7d18e77f5c564e06b5a5135f02bfc2624a9003ed
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-04-16 16:54:04 -0300

    Página Inicial 30%

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -2,18 +2,51 @@ import useMediaQuery from "@/hooks/useMediaQuery";
 import { SelectedPage } from "@/shared/types";
 import React from "react";
 import ActionButton from "@/shared/ActionButton";
-import HomePageText from "@/assets/HomePageText.png";
+import HomePageText from "@/assets/page.png";
 import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Home = ({ setSelectedPage }: Props) => {
-  return <div>Página Inicial</div>;
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  return (
+    <section
+      id="paginainicial"
+      className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0"
+    >
+      <div>
+        <div>
+          <div>
+            <div>
+              <div>
+                <img src={HomePageText} alt="Img Página Inicial" />
+              </div>
+            </div>
+            <p>Aqui será onde você vai encontrar os melhores produtos!</p>
+          </div>
+          <div>
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Entrar
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.Contato)}
+              href={`#${SelectedPage.Contato}`}
+            ></AnchorLink>
+          </div>
+        </div>
+
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
